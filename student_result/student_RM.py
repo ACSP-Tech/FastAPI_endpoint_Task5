855b959767092f8657f989b7e8866ee99e295890
Precious Aguluka <agulukachioma@gmail.com>
2025-08-13 17:52:44 -0700

    push after Student Results endpoints

---
+++ student_result/student_RM.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+from students_utils import check_filepath,compute_grade
+import json
+import os
+
+app = FastAPI()
+
+name = "placeholder"
+subject_scores = 0.0
+average = 0.0
+
+#post request for adding students
+class Student(BaseModel):
+    name: str
+    subject_scores: float
+    average: float
+
+
+class StudentOut(BaseModel):
+    name: str
+    subject_scores: float
+    average: float
+    grade: str
+
+@app.post("/students/",  response_model=StudentOut, status_code=status.HTTP_201_CREATED)
+def add_student(data:Student):
+    try:
+        check_filepath() 
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(BASE_DIR, "student_result.json")
+        with open(file_path, "r") as file:
+            old_json = json.load(file)
+        # Calculate grade based on score
+        total = data.average * 2
+        grade_score = (data.subject_scores/total) * 100
+        grade = compute_grade(grade_score)
+        student_out = StudentOut(
+            name=data.name,
+            subject_scores=data.subject_scores,
+            average=data.average,
+            grade=grade
+        )
+        old_json.append(student_out.model_dump())
+        with open(file_path, "w") as file:
+            json.dump(old_json, file)
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Ensure subject_scores and average are floats"
+        )
+    except Exception as e:
+        # Catches all other unexpected errors and shows them
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Unexpected error: {e}")
+    return student_out
+    
+@app.get("/students/{name}")
+def filter_db(name:str):
+    try:
+        check_filepath()
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(BASE_DIR, "student_result.json")
+        with open(file_path, "r") as file:
+            old_json = json.load(file)
+        #case - insensititive match
+        student_record = [student for student in old_json if student["name"].lower() == name.lower()]
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Student name not found/ does not exist")
+    return student_record
+
+@app.get("/students/")
+def view_db():
+    try:
+        check_filepath()
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(BASE_DIR, "student_result.json")
+        with open(file_path, "r") as file:
+            old_json = json.load(file)
+        #case - insensititive match
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Student name not found/ does not exist")
+    return old_json
---
+++ student_result/students_utils.py
@@ -0,0 +1,27 @@
+from fastapi import HTTPException, status
+
+def check_filepath():
+    import os
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    try:
+        import json
+        file_path1 = os.path.join(BASE_DIR, "student_result.json")
+        if not os.path.exists(file_path1):
+            with open(file_path1, "x") as file:
+                json.dump([], file)
+    except:
+       raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Error Creating Json file")
+    
+def compute_grade(grade_score: float) -> str:
+    if grade_score >= 90:
+        return "A"
+    elif grade_score >= 80:
+        return "B"
+    elif grade_score >= 70:
+        return "C"
+    elif grade_score >= 60:
+        return "D"
+    elif grade_score >= 50:
+        return "E"
+    else:
+        return "F"
